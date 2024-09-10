aca8cf1363c26acb3417a64b2aa2572b6c9b307e
Onixdrew <andrewmosquera1811@gmail.com>
2024-09-09 21:05:00 -0500

    'Updated'

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,50 @@
-// App.jsx
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import InicioIntructor from "./pages/interfaces/Instructor/inicioIntructor.jsx";
-import ReportesInstructor from "./pages/interfaces/Instructor/reportesInstructor.jsx";
-import GraficasInstructor from "./pages/interfaces/Instructor/Graficas/graficas.jsx";
-
-import GuardiaHome from "./pages/interfaces/Guarda/GuardaHome.jsx";
-import Sobrenosotros from "./pages/interfaces/Guarda/Sobrenosotros.jsx";
-import InicioGuardia from "./pages/interfaces/Guarda/RegistroPersona.jsx";
-import Login from "./pages/Login/Login.jsx";
-import InicioAdmin from "./pages/interfaces/Administrador/inicioAdmin.jsx";
-import ReportesAdmin from "./pages/interfaces/Administrador/reportesAdmin.jsx";
-import GraficasAdmin from "./pages/interfaces/Administrador/Graficas/graficas.jsx";
-import Home from "./pages/Lobby/home.jsx";
 import AuthProvider from "./Context/AuthContext.jsx";
 import RutasProtegidas from "./auth/authRoutes.jsx";
+
+// Importaciones dinámicas
+const InicioIntructor = lazy(() => import("./pages/interfaces/Instructor/inicioIntructor.jsx"));
+const ReportesInstructor = lazy(() => import("./pages/interfaces/Instructor/reportesInstructor.jsx"));
+const GraficasInstructor = lazy(() => import("./pages/interfaces/Instructor/Graficas/graficas.jsx"));
+
+const GuardiaHome = lazy(() => import("./pages/interfaces/Guarda/GuardaHome.jsx"));
+const Sobrenosotros = lazy(() => import("./pages/interfaces/Guarda/Sobrenosotros.jsx"));
+const InicioGuardia = lazy(() => import("./pages/interfaces/Guarda/RegistroPersona.jsx"));
+const Login = lazy(() => import("./pages/Login/Login.jsx"));
+const InicioAdmin = lazy(() => import("./pages/interfaces/Administrador/inicioAdmin.jsx"));
+const ReportesAdmin = lazy(() => import("./pages/interfaces/Administrador/reportesAdmin.jsx"));
+const GraficasAdmin = lazy(() => import("./pages/interfaces/Administrador/Graficas/graficas.jsx"));
+const Home = lazy(() => import("./pages/Lobby/home.jsx"));
 
 const App = () => (
   <AuthProvider>
     <BrowserRouter>
-      <Routes>
-        {/* Publicas */}
-        <Route path="/" element={<Home />} />
-        <Route path="/Login" element={<Login />} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {/* Publicas */}
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
 
-        {/* privadas */}
-        <Route element={<RutasProtegidas/>}>
-          {/* Guardia */}
-          <Route path="/InicioGuardia" element={<GuardiaHome />} />
-          <Route path="/ReconocimientoGuardia" element={<InicioGuardia />} />
-          <Route path="/Mas" element={<Sobrenosotros />} />
+          {/* Privadas */}
+          <Route element={<RutasProtegidas />}>
+            {/* Guardia */}
+            <Route path="/InicioGuardia" element={<GuardiaHome />} />
+            <Route path="/ReconocimientoGuardia" element={<InicioGuardia />} />
+            <Route path="/Mas" element={<Sobrenosotros />} />
 
-          {/* Instructor */}
-          <Route path="/inicioInstructor" element={<InicioIntructor />} />
-          <Route path="/ReportesInstructor" element={<ReportesInstructor />} />
-          <Route path="/GraficasInstructor" element={<GraficasInstructor />} />
+            {/* Instructor */}
+            <Route path="/inicioInstructor" element={<InicioIntructor />} />
+            <Route path="/ReportesInstructor" element={<ReportesInstructor />} />
+            <Route path="/GraficasInstructor" element={<GraficasInstructor />} />
 
-          {/* Administrador */}
-          <Route path="/inicioAdmin" element={<InicioAdmin />} />
-          <Route path="/ReportesAdmin" element={<ReportesAdmin />} />
-          <Route path="/GraficasAdmin" element={<GraficasAdmin />} />
-        </Route>
-      </Routes>
+            {/* Administrador */}
+            <Route path="/inicioAdmin" element={<InicioAdmin />} />
+            <Route path="/ReportesAdmin" element={<ReportesAdmin />} />
+            <Route path="/GraficasAdmin" element={<GraficasAdmin />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   </AuthProvider>
 );
